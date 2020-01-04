d7dff096ea4e3e5a9ef4d73384ca40ea18801684
pedrojunqueira <pedrocj@gmail.com>
2020-01-04 20:10:55 +1030

    heroku deployment

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pedropythondev@gmail.com']
     POSTS_PER_PAGE = 10
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
